5458e3ca52a43fd50f6e3fc99f21f699cd24c8c8
Manthan Baranwal <manthan.baranwal@appveen.com>
2020-12-14 16:49:45 +0530

    hotfix-DEF3228

--- util/codegen/projectSkeletons/templates/controller.bulkUpload.template.js
+++ util/codegen/projectSkeletons/templates/controller.bulkUpload.template.js
@@ -1124,32 +1124,32 @@ const functiontoParseBooleans = (definition, pathAsOfNow) => {
 				functiontoParseBooleans(currentKeyValue.definition, newPath);
 		}
 		if (currentKeyValue.type === 'Boolean') {
-			code.push(`\tlet ${_.camelCase(newPath)} = _.get(newData, '${newPath}');`);
-			code.push(`if(typeof ${_.camelCase(newPath)} === 'string' && (${_.camelCase(newPath)} === 'Y' || ${_.camelCase(newPath)} === 'y')){`);
-			code.push(` ${_.camelCase(newPath)} = true;`);
+			code.push(`\tlet var_${_.camelCase(newPath)} = _.get(newData, '${newPath}');`);
+			code.push(`if(typeof var_${_.camelCase(newPath)} === 'string' && (var_${_.camelCase(newPath)} === 'Y' || var_${_.camelCase(newPath)} === 'y')){`);
+			code.push(` var_${_.camelCase(newPath)} = true;`);
 			code.push('}');
-			code.push(`if(typeof ${_.camelCase(newPath)} === 'string' && (${_.camelCase(newPath)} === 'N' || ${_.camelCase(newPath)} === 'n')){`);
-			code.push(` ${_.camelCase(newPath)} = false;`);
+			code.push(`if(typeof var_${_.camelCase(newPath)} === 'string' && (var_${_.camelCase(newPath)} === 'N' || var_${_.camelCase(newPath)} === 'n')){`);
+			code.push(` var_${_.camelCase(newPath)} = false;`);
 			code.push('}');
-			code.push(`if(typeof ${_.camelCase(newPath)} === 'string'){`);
-			code.push(` ${_.camelCase(newPath)} = ${_.camelCase(newPath)}.toLowerCase();`);
+			code.push(`if(typeof var_${_.camelCase(newPath)} === 'string'){`);
+			code.push(` var_${_.camelCase(newPath)} = var_${_.camelCase(newPath)}.toLowerCase();`);
 			code.push('}');
-			code.push(`\t_.set(newData, '${newPath}', ${_.camelCase(newPath)});\n`);
+			code.push(`\t_.set(newData, '${newPath}', var_${_.camelCase(newPath)});\n`);
 		}
 
 		if (currentKeyValue.type === 'Array') {
 			if (currentKeyValue.definition._self.type === 'Object') {
-				code.push(`\tlet ${_.camelCase(newPath)} = _.get(newData, '${newPath}');`);
-				code.push(`if (Array.isArray(${_.camelCase(newPath)})) {`);
-				code.push(`    ${_.camelCase(newPath)}.forEach(newData => {`);
+				code.push(`\tlet var_${_.camelCase(newPath)} = _.get(newData, '${newPath}');`);
+				code.push(`if (Array.isArray(var_${_.camelCase(newPath)})) {`);
+				code.push(`    var_${_.camelCase(newPath)}.forEach(newData => {`);
 				functiontoParseBooleans(currentKeyValue.definition._self.definition, '');
 				code.push('    })');
 				code.push('}');
-				code.push(`\t_.set(newData, '${newPath}', ${_.camelCase(newPath)});\n`);
+				code.push(`\t_.set(newData, '${newPath}', var_${_.camelCase(newPath)});\n`);
 			} else if (currentKeyValue.definition._self.type === 'Boolean') {
-				code.push(`\tlet ${_.camelCase(newPath)} = _.get(newData, '${newPath}');`);
-				code.push(`if (Array.isArray(${_.camelCase(newPath)})) {`);
-				code.push(`    ${_.camelCase(newPath)} = ${_.camelCase(newPath)}.map(i => {`);
+				code.push(`\tlet var_${_.camelCase(newPath)} = _.get(newData, '${newPath}');`);
+				code.push(`if (Array.isArray(var_${_.camelCase(newPath)})) {`);
+				code.push(`    var_${_.camelCase(newPath)} = var_${_.camelCase(newPath)}.map(i => {`);
 				code.push('        if (typeof i === \'string\' && (i === \'Y\' || i === \'y\')) {');
 				code.push('            return i = true');
 				code.push('        }');
@@ -1162,7 +1162,7 @@ const functiontoParseBooleans = (definition, pathAsOfNow) => {
 				code.push('        return i;');
 				code.push('    })');
 				code.push('}');
-				code.push(`\t_.set(newData, '${newPath}', ${_.camelCase(newPath)});\n`);
+				code.push(`\t_.set(newData, '${newPath}', var_${_.camelCase(newPath)});\n`);
 			}
 		}
 	}
