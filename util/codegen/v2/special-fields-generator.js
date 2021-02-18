ccee21a6e2061574c6aece55d8fffe445dbb52e0
Jugnu Agrawal <jugnu@appveen.com>
2021-02-18 16:43:51 +0530

    lint fix

--- util/codegen/v2/special-fields-generator.js
+++ util/codegen/v2/special-fields-generator.js
@@ -599,7 +599,7 @@ function genrateCode(config) {
 					code.push('\t\t\t} else if (_.indexOf(falseBooleanValues, ${_.camelCase(path)}) > -1) {');
 					code.push(`\t\t\t\t_.set(newData, '${path}', false);`);
 					code.push('\t\t\t} else {');
-					code.push(`\t\t\t\tthrow new Error('Invalid Boolean Value');`);
+					code.push('\t\t\t\tthrow new Error(\'Invalid Boolean Value\');');
 					code.push('\t\t\t}');
 					code.push('\t\t}');
 					code.push('\t} catch (e) {');
@@ -614,7 +614,7 @@ function genrateCode(config) {
 						code.push(`\tif (${_.camelCase(path)} && Array.isArray(${_.camelCase(path)}) && ${_.camelCase(path)}.length > 0) {`);
 						code.push(`\t\t${_.camelCase(path)} = ${_.camelCase(path)}.map((item, i) => {`);
 						code.push('\t\t\ttry {');
-						code.push(`\t\t\t\tif (typeof item == 'number' || typeof item == 'boolean') {`);
+						code.push('\t\t\t\tif (typeof item == \'number\' || typeof item == \'boolean\') {');
 						code.push('\t\t\t\t\titem = item.toString();');
 						code.push('\t\t\t\t}');
 						code.push('\t\t\t\tif (typeof item == \'string\' && _.indexOf(trueBooleanValues, item.toLowerCase()) > -1) {');
@@ -622,7 +622,7 @@ function genrateCode(config) {
 						code.push('\t\t\t\t} else if (typeof item == \'string\' && _.indexOf(falseBooleanValues, item.toLowerCase()) > -1){');
 						code.push('\t\t\t\t\treturn false;');
 						code.push('\t\t\t\t} else {');
-						code.push(`\t\t\t\t\tthrow new Error('Invalid Boolean Value');`);
+						code.push('\t\t\t\t\tthrow new Error(\'Invalid Boolean Value\');');
 						code.push('\t\t\t\t}');
 						code.push('\t\t\t} catch (e) {');
 						code.push(`\t\t\t\terrors['${path}.' + i] = e;`);
