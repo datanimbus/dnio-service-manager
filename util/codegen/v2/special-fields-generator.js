00ec5e62e80a0f2ba88a6af60e3e26e933fba452
Jugnu Agrawal <jugnu@appveen.com>
2021-03-26 13:08:05 +0530

    using lodash unset

--- util/codegen/v2/special-fields-generator.js
+++ util/codegen/v2/special-fields-generator.js
@@ -259,7 +259,7 @@ function genrateCode(config) {
 						if(def.definition[0].properties.relatedTo)
 							code.push(`\t\t\t\tconst doc = await commonUtils.getServiceDoc(req, '${def.definition[0].properties.relatedTo}', item._id);`);
 						else
-							code.push('\t\t\t\tconst doc = await commonUtils.getUserDoc(req, item._id);');
+							code.push(`\t\t\t\tconst doc = await commonUtils.getUserDoc(req, item._id);`);
 						code.push('\t\t\t\t\tif (!doc) {');
 						code.push(`\t\t\t\t\t\terrors['${path}.' + i] = item._id + ' not found';`);
 						code.push('\t\t\t\t\t} else {');
@@ -410,20 +410,24 @@ function genrateCode(config) {
 					code.push('\t\tif (!forceRemove) {');
 					code.push(`\t\t\tif (_.differenceWith((_.get(newData, '${path}')||[]), (_.get(oldData, '${path}')||[]), _.isEqual)) {`);
 					// code.push(`\t\t\t\terrors['${path}'] = '${path} field cannot be updated, Violation of Create Only';`);
-					code.push(`\t\t\tdelete newData['${path}'];`);
+					// code.push(`\t\t\tdelete newData['${path}'];`);
+					code.push(`\t\t\t_.unset(newData, '${path}');`);
 					code.push('\t\t\t}');
 					code.push('\t\t} else {');
-					code.push(`\t\t\tdelete newData['${path}'];`);
+					// code.push(`\t\t\tdelete newData['${path}'];`);
+					code.push(`\t\t\t_.unset(newData, '${path}');`);
 					code.push('\t\t}');
 				} else {
 					if (def.properties.createOnly) {
 						code.push('\t\tif (!forceRemove) {');
 						code.push(`\t\t\tif (_.get(newData, '${path}') !== _.get(oldData, '${path}')) {`);
 						// code.push(`\t\t\t\terrors['${path}'] = '${path} field cannot be updated, Violation of Create Only';`);
-						code.push(`\t\t\tdelete newData['${path}'];`);
+						// code.push(`\t\t\tdelete newData['${path}'];`);
+						code.push(`\t\t\t_.unset(newData, '${path}');`);
 						code.push('\t\t\t}');
 						code.push('\t\t} else {');
-						code.push(`\t\t\tdelete newData['${path}'];`);
+						// code.push(`\t\t\tdelete newData['${path}'];`);
+						code.push(`\t\t\t_.unset(newData, '${path}');`);
 						code.push('\t\t}');
 					}
 				}
