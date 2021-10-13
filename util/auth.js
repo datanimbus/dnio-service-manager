3d3a153e784213a88b11754b915f06838d9b8553
Jugnu Agrawal <jugnu@appveen.com>
2021-10-13 16:24:06 +0530

    using auth-cache MW

--- util/auth.js
+++ util/auth.js
@@ -1,12 +1,9 @@
-const crypto = require('crypto');
-const JWT = require('jsonwebtoken');
 const router = require('express').Router();
-const AuthCache = require('@appveen/ds-auth-cache');
+const { AuthCacheMW } = require('@appveen/ds-auth-cache');
 const _ = require('lodash');
 const config = require('../config/config');
 
 const logger = global.logger;
-const cache = new AuthCache();
 
 const permittedUrls = [
     "/api/a/rbac/login",
@@ -23,44 +20,7 @@ const permittedUrls = [
     "/service/{id}/checkUnique"
 ];
 
-router.use(async (req, res, next) => {
-    try {
-        if (permittedUrls.some(_url => conpareURL(_url, req.path)) || req.path.indexOf('/health') > -1 || req.path.indexOf('/export') > -1) {
-            return next();
-        }
-
-        logger.debug(`[${req.header('txnId')}] Validating token format`);
-        let token = req.header('authorization');
-
-        if (!token) {
-            logger.debug(`[${req.header('txnId')}] No token found in 'authorization' header`);
-            logger.debug(`[${req.header('txnId')}] Checking for 'authorization' token in cookie`);
-            token = req.cookies.Authorization;
-        }
-
-        if (!token) return res.status(401).json({ message: 'Unauthorized' });
-
-        token = token.split('JWT ')[1];
-        const user = JWT.verify(token, config.TOKEN_SECRET, { ignoreExpiration: true });
-        if (!user) {
-            logger.error(`[${req.header('txnId')}] Invalid JWT format`);
-            return res.status(401).json({ 'message': 'Unauthorized' });
-        }
-        let tokenHash = md5(token);
-        logger.debug(`[${req.header('txnId')}] Token hash :: ${tokenHash}`);
-        req.tokenHash = tokenHash;
-        req.user = typeof user === 'string' ? JSON.parse(user) : user;
-        logger.trace(`[${req.header('txnId')}] Token Data : ${JSON.stringify(req.user)}`);
-
-        // Fetching from Redis Cache
-        const permissions = await cache.getUserPermissions(req.user._id);
-        req.user.permissions = permissions || [];
-        next();
-    } catch (err) {
-        logger.err('[Auth]', err);
-        res.status(500).json({ message: err.message });
-    }
-});
+router.use(AuthCacheMW({ permittedUrls, secret: config.TOKEN_SECRET, decodeOnly: true }));
 
 router.use(['/sm/service', '/sm/service/.*', '/:id/draftDelete', '/:id/purge/all', '/:id/purge/:type', '/:id/count', '/:id/:app/idCount', '/calendar]enable', '/calendar/disable', '/logs', '/:id/lockDocument/count', '/tags'], async (req, res, next) => {
     if (!req.user.permissions.some(e => e.startsWith('PMDS') || e.startsWith('PVDS'))) {
@@ -131,25 +91,5 @@ router.use(['/sm/app/:app'], async (req, res, next) => {
     }
     next();
 });
-
-function md5(text) {
-    return crypto.createHash('md5').update(text).digest('hex');
-}
-
-function conpareURL(tempUrl, url) {
-    let tempUrlSegment = tempUrl.split("/").filter(_d => _d != "");
-    let urlSegment = url.split("/").filter(_d => _d != "");
-    if (tempUrlSegment.length != urlSegment.length) return false;
-
-    tempUrlSegment.shift();
-    urlSegment.shift();
-
-    let flag = tempUrlSegment.every((_k, i) => {
-        if (_k.startsWith("{") && _k.endsWith("}") && urlSegment[i] != "") return true;
-        return _k === urlSegment[i];
-    });
-    logger.trace(`Compare URL :: ${tempUrl}, ${url} :: ${flag}`);
-    return flag;
-}
 
 module.exports = router;
