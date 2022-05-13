8bce81e21412b8f3afc8799ca6dfce6f7359a152
Jugnu Agrawal <jugnu@appveen.com>
2022-05-13 12:48:30 +0530

    jenkins test

---
+++ scripts/echo.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo $branch
+echo $tag
+echo $dockerHub
+echo $ecr
+echo $gcr
+echo $deploy
+
+
+echo ${params.branch}
+echo ${params.tag}
+echo ${params.dockerHub}
+echo ${params.ecr}
+echo ${params.gcr}
+echo ${params.deploy}
