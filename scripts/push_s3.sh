6ac1d17a733ba8dac48421f99cf9718aabb45514
Jugnu Agrawal <jugnu@appveen.com>
2022-05-16 12:49:07 +0530

    S3 push in today's folder

--- scripts/push_s3.sh
+++ scripts/push_s3.sh
@@ -8,9 +8,11 @@ echo "****************************************************"
 echo "data.stack:sm :: Saving Image to AWS S3 :: $S3_BUCKET/stable-builds"
 echo "****************************************************"
 
+TODAY_FOLDER=`date ++%Y_%m_%d`
+
 docker save -o data.stack.sm_$TAG.tar data.stack.sm:$TAG
 bzip2 data.stack.sm_$TAG.tar
-aws s3 cp data.stack.sm_$TAG.tar.bz2 s3://$S3_BUCKET/stable-builds/data.stack.sm_$TAG.tar.bz2
+aws s3 cp data.stack.sm_$TAG.tar.bz2 s3://$S3_BUCKET/stable-builds/$TODAY_FOLDER/data.stack.sm_$TAG.tar.bz2
 rm data.stack.sm_$TAG.tar.bz2
 
 echo "****************************************************"
