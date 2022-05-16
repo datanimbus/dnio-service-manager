1a9da4dfd0848ad6ba11a179d81aa59b3ee8ba23
Jugnu Agrawal <jugnu@appveen.com>
2022-05-16 17:02:40 +0530

    ecr login

--- scripts/push_ecr.sh
+++ scripts/push_ecr.sh
@@ -9,7 +9,7 @@ echo "****************************************************"
 echo "data.stack:sm :: Pushing Image to ECR :: $ECR_URL/data.stack.sm:$TAG"
 echo "****************************************************"
 
-aws ecr get-login --no-include-email
+$(aws ecr get-login --no-include-email)
 docker tag data.stack.sm:$TAG $ECR_URL/data.stack.sm:$TAG
 docker push $ECR_URL/data.stack.sm:$TAG
 
