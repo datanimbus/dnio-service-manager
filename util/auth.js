4460edbbe9ed459d660b3859e947e1363306e34e
kavi-appveen <kavi@appveen.com>
2023-07-26 18:28:28 +0530

    fix

--- util/auth.js
+++ util/auth.js
@@ -99,7 +99,7 @@ router.use((req, res, next) => {
 		}
 
 		if (req.locals.app && params && req.locals.app !== params['{app}']) {
-			return next(new Error("Appname in url and body/filter do not match."));
+			return next(new Error("App in url does not match with one in either body or filter."));
 		}
 		
 		if (!req.locals.app && params && params['{app}']) req.locals.app = params['{app}'];
