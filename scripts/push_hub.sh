27ac5b2d9a1f577f5cc6be03970ed67cf3ddcb31
Jugnu Agrawal <jugnu@appveen.com>
2023-09-20 14:54:17 +0530

    dockerhub change

--- scripts/push_hub.sh
+++ scripts/push_hub.sh
@@ -5,19 +5,19 @@ set -e
 TAG=`cat CURRENT_SM`
 
 echo "****************************************************"
-echo "datanimbus.io.sm :: Pushing Image to Docker Hub :: appveen/datanimbus.io.sm:$TAG"
+echo "datanimbus.io.sm :: Pushing Image to Docker Hub :: datanimbus/datanimbus.io.sm:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.sm:$TAG appveen/datanimbus.io.sm:$TAG
-docker push appveen/datanimbus.io.sm:$TAG
+docker tag datanimbus.io.sm:$TAG datanimbus/datanimbus.io.sm:$TAG
+docker push datanimbus/datanimbus.io.sm:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.sm :: Image Pushed to Docker Hub AS appveen/datanimbus.io.sm:$TAG"
+echo "datanimbus.io.sm :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.sm:$TAG"
 echo "****************************************************"
 
-docker tag datanimbus.io.base:$TAG appveen/datanimbus.io.base:$TAG
-docker push appveen/datanimbus.io.base:$TAG
+docker tag datanimbus.io.base:$TAG datanimbus/datanimbus.io.base:$TAG
+docker push datanimbus/datanimbus.io.base:$TAG
 
 echo "****************************************************"
-echo "datanimbus.io.sm :: Image Pushed to Docker Hub AS appveen/datanimbus.io.base:$TAG"
+echo "datanimbus.io.sm :: Image Pushed to Docker Hub AS datanimbus/datanimbus.io.base:$TAG"
 echo "****************************************************"
